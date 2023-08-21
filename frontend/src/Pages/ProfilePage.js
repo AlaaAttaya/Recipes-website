09c8e3376772914fed529039f55d3862b39aab5f
Alaa Attaya <attayaalaa@gmail.com>
2023-08-21 16:57:29 +0300

    Frontend <> Implement API in EditProfile Page

--- frontend/src/Pages/ProfilePage.js
+++ frontend/src/Pages/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../styles/styles.css";
 import axios from "axios";
 import Avatar from "../Components/Avatar";
@@ -9,6 +9,13 @@ const ProfilePage = () => {
   const [imageFile, setImageFile] = useState(null);
   const imageInputRef = useRef();
   const [isLeftDivOpen, setIsLeftDivOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [errorMsgProfile, setErrorMsgProfile] = useState("");
+  const [user, setUserProfile] = useState(null);
 
   const handleHome = () => {
     window.location.replace("/Home");
@@ -31,10 +38,95 @@ const ProfilePage = () => {
     }
   };
 
+  const handleEditProfile = async () => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("username", username);
+    if (imageFile) {
+      formData.append("image", imageFile);
+    }
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/user/editprofile",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setErrorMsgProfile("Success");
+    } catch (error) {
+      setErrorMsgProfile("Failed To Change Pic/Username/Name");
+    }
+  };
+
+  const handleChangePassword = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/user/changepassword",
+        {
+          oldpassword: oldPassword,
+          newpassword: newPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      const userData1 = await response.data;
+
+      console.log(userData1);
+      if (
+        userData1.message ==
+          "The newpassword field must be at least 8 characters." ||
+        userData1.message == "Incorrect old password"
+      ) {
+        setErrorMsg("Failed To Change Password ");
+      } else {
+        setErrorMsg("Success");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+      setErrorMsg("Failed To Change  Password");
+    }
+  };
+
   if (!localStorage.getItem("token")) {
     window.location.replace("/");
   }
 
+  const fetchUserProfile = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/user/profile", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const userData = await response.json();
+
+      setUserProfile(userData);
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+      localStorage.removeItem("token");
+      window.location.replace("/");
+    }
+  };
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+  useEffect(() => {
+    if (user) {
+      setImage(`http://127.0.0.1:8000${user.data.image}`);
+      setName(user.data.name);
+      setUsername(user.data.username);
+    }
+  }, [user]);
   return (
     <>
       <div className="editProfile ">
@@ -95,10 +187,23 @@ const ProfilePage = () => {
               />
             </div>
             <label>Username</label>
-            <input type="text" placeholder="UserName" style={{ height: 20 }} />
+            <input
+              type="text"
+              placeholder="Username"
+              style={{ height: 20 }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
             <label>Name</label>
-            <input type="text" placeholder="Name" style={{ height: 20 }} />
-            <button>Save</button>
+            <input
+              type="text"
+              placeholder="Name"
+              style={{ height: 20 }}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label className="error-message">{errorMsgProfile}</label>
+            <button onClick={handleEditProfile}>Save Profile</button>
 
             <hr style={{ marginTop: "30px", marginBottom: "30px" }}></hr>
             <label>Old Password</label>
@@ -106,15 +211,19 @@ const ProfilePage = () => {
               type="password"
               placeholder="Old Password"
               style={{ height: 20 }}
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
             />
             <label>New Password</label>
             <input
               type="password"
               placeholder="New Password"
               style={{ height: 20 }}
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
             />
-            <button>Save</button>
-            <label className="error-message"></label>
+            <button onClick={handleChangePassword}>Save Password</button>
+            <label className="error-message">{errorMsg}</label>
           </div>
         </div>
       </div>
